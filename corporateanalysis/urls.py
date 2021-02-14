9aa51a290945be688d89b57e897ad9eb43b97c4c
chocobaby727 <chocobabion1152@gmail.com>
2021-02-14 14:09:52 +0900

    検索画面とりあえず完成

--- corporateanalysis/urls.py
+++ corporateanalysis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, corporateanalysis
 
 app_name = 'corporateanalysis'
 
 urlpatterns = [
     path('', hello_world, name='hello'),
+    path('search/search.html', corporateanalysis, name='search'),
 ]
