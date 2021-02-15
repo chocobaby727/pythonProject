6d33b2f5b352b4f62fe7ccaab7dc633d41bce3d9
Soshi Homma <ilovenewtechs@gmail.com>
2021-02-15 22:37:07 +0900

    owari

--- corporateanalysis/urls.py
+++ corporateanalysis/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import company_info, hello_world
-=======
-from .views import hello_world, corporateanalysis
->>>>>>> master
+from .views import company_info, hello_world, corporateanalysis
 
 app_name = 'corporateanalysis'
 
 urlpatterns = [
     path('', hello_world, name='hello'),
-<<<<<<< HEAD
     path('info/', company_info, name='info'),
-=======
     path('search/search.html', corporateanalysis, name='search'),
->>>>>>> master
 ]
