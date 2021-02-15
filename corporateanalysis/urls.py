6e1800cbc80bddb58cefe744f67a7778abd080ae
chocobaby727 <chocobabion1152@gmail.com>
2021-02-15 23:18:46 +0900

    分析開始（カテゴリに分かれている）画面完成

--- corporateanalysis/urls.py
+++ corporateanalysis/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import hello_world, corporateanalysis
+from .views import hello_world, search, comparison
 
 app_name = 'corporateanalysis'
 
 urlpatterns = [
     path('', hello_world, name='hello'),
-    path('search/search.html', corporateanalysis, name='search'),
+    path('search/search.html', search, name='search'),
+    path('comparison/comparison_menu.html', comparison, name='comparison_menu'),
 ]
--- corporateanalysis/views.py
+++ corporateanalysis/views.py
@@ -8,5 +8,9 @@ def hello_world(request):
     return render(request, template_name='corporateanalysis/hello.html')
 
 
-def corporateanalysis(request):
+def search(request):
     return render(request, template_name='corporateanalysis/search/search.html')
+
+
+def comparison(request):
+    return render(request, template_name='corporateanalysis/comparison/comparison_menu.html')
