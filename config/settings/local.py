51abf76776e01d815db734f3fab8b297f1dcd67b
chocobaby727 <chocobabion1152@gmail.com>
2021-02-18 23:00:07 +0900

    Proc更新

--- config/settings/local.py
+++ config/settings/local.py
@@ -19,21 +19,21 @@ ALLOWED_HOSTS = ['*']
 #########
 # Debug #
 #########
-if DEBUG:
-    def show_toolbar(request):
-        return True
-
-
-    INSTALLED_APPS += (
-        'debug_toolbar',
-    )
-    MIDDLEWARE += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
-    DEBUG_TOOLBAR_CONFIG = {
-        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
-    }
-
+# if DEBUG:
+#     def show_toolbar(request):
+#         return True
+#
+#
+#     INSTALLED_APPS += (
+#         'debug_toolbar',
+#     )
+#     MIDDLEWARE += (
+#         'debug_toolbar.middleware.DebugToolbarMiddleware',
+#     )
+#     DEBUG_TOOLBAR_CONFIG = {
+#         'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+#     }
+#
 
 ############
 # Database #
